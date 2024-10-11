6c2c1c034400d3828471a3701a8eff9b77c327aa
James Kemboi <jameskemich@gmail.com>
2024-10-11 23:29:14 +0300

    Added MongoDb database

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -32,7 +32,6 @@ const navigate = useNavigate()
 const handleSubmit = async(e) => {
     e.preventDefault()
     const response = await fetch('http://localhost:5000/api/bookings', {
-
     method: 'POST',
     headers: {'Content-Type':'application/json',
     },
