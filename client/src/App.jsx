092beb7daf770702e62c7dc85be469bf419fd802
James Kemboi <jameskemich@gmail.com>
2024-10-29 23:01:25 +0300

    refined footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,11 +24,49 @@ function App() {
 
    
     <ScrollToTopButton />
-      <footer>
-    
-  <p>Contact: (555) 123-4567 | info@qwinnissalon.com
-    © 2024 Qwinnis Hair Salon. All rights reserved</p>
-    </footer>
+
+    <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-column">
+                    <h3>Contact & Location</h3>
+                    <p>Phone: <a href="tel:+15551234567">(555) 123-4567</a></p>
+                    <p>Email: <a href="mailto:info@qwinnissalon.com">info@qwinnissalon.com</a></p>
+                    <p>Address: 123 Main St, Torongo</p>
+                </div>
+
+                <div className="footer-column">
+                    <h3>Social Media</h3>
+                 
+                   <li><a href="/about">About Us</a></li>
+                        <li><a href="/services">Services</a></li>
+                        <li><a href="#">Customer Support</a></li>
+                        <li><a href="#">Special Offers</a></li>
+                   
+                </div>
+
+                <div className="footer-column">
+                    <h3>Opening Hours</h3>
+                    <p>Monday - Friday: 9 AM - 7 PM</p>
+                    <p>Saturday: 10 AM - 5 PM</p>
+                    <p>Sunday: Closed</p>
+                </div>
+
+                <div className="footer-column footer__socials">
+                    <h3>Social Media</h3>
+                  
+                        <li><a href="https://www.facebook.com/qwinnissalon" target="_blank" rel="noopener noreferrer"><i class="ri-facebook-fill"></i></a></li>
+                        <li><a href="https://www.instagram.com/qwinnissalon" target="_blank" rel="noopener noreferrer"><i class="ri-instagram-line"></i></a></li>
+                        <li><a href="https://www.youtube.com/qwinnissalon" target="_blank" rel="noopener noreferrer"><i class="ri-youtube-fill"></i></a></li>
+                        <li><a href="https://www.linkedin.com/qwinnissalon" target="_blank" rel="noopener noreferrer"><i class="ri-linkedin-fill"></i></a></li>
+                       
+                    
+                </div>
+
+            </div>
+            <div className="footer-bottom">
+                <p>© 2024 Qwinnis Hair Salon. All rights reserved.</p>
+            </div>
+        </footer>
        
      
    </div>
